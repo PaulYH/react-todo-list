89cc13db52c0a599af85779c9181c8aa33df34fe
PaulYH <paulhumenny@icloud.com>
2022-09-16 16:04:09 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./App.css";
+
+//Imported Components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <header>
+        <h1>Paul's Todo List</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
